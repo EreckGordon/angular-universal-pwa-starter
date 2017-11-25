03bf46a53256d8a9208a5b1b75f4828e63084c93
EreckGordon <ereckgordon@gmail.com>
2017-11-24 17:29:00 -0700

    add body parser

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+
+import * as bodyParser from 'body-parser';
+
 import { ApplicationModule } from './modules/app.module';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
+  app.use(bodyParser.json());
   await app.listen(8000);
 }
+
 bootstrap();
