5a0a695c40daea0e0f4a85bb8f0952d27009a60e
EreckGordon <ereckgordon@gmail.com>
2017-11-24 17:28:01 -0700

    article module

---
+++ server/modules/article/article.providers.ts
@@ -0,0 +1,10 @@
+import { Connection, Repository } from 'typeorm';
+import { Article } from './article.entity';
+
+export const articleProviders = [
+  {
+    provide: 'ArticleRepositoryToken',
+    useFactory: (connection: Connection) => connection.getRepository(Article),
+    inject: ['DbConnectionToken'],
+  },
+];
