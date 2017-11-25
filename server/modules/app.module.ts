321a9a0f65ad440301610d31256e67ec24cba515
EreckGordon <ereckgordon@gmail.com>
2017-11-24 17:28:28 -0700

    auth only injects to api

--- server/modules/app.module.ts
+++ server/modules/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { APIModule } from './api/api.module';
-import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-    modules: [APIModule, AuthModule],
+    modules: [APIModule],
 })
 export class ApplicationModule {}
