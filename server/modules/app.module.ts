4bcb992c988dcc2a85e07ae3d93c8a7a415e6f81
EreckGordon <ereckgordon@gmail.com>
2017-11-25 10:16:11 -0700

    working server render
    i think

--- server/modules/app.module.ts
+++ server/modules/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { APIModule } from './api/api.module';
+import { AppController } from './app.controller';
 
 
 @Module({
     modules: [APIModule],
+    controllers: [AppController]
 })
 export class ApplicationModule {}
--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,57 @@
-import { NestFactory } from '@nestjs/core';
+import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
+import 'ts-helpers';
 
+import { NestFactory } from '@nestjs/core';
+import { enableProdMode } from '@angular/core';
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+
+import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
+import * as path from 'path';
+import * as compression from 'compression';
 
 import { ApplicationModule } from './modules/app.module';
 
 
+const server = express();
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('../dist-server/main.bundle');
+enableProdMode();
+const configuredNgExpressEngine = ngExpressEngine({
+  bootstrap: AppServerModuleNgFactory,
+  providers: [
+    provideModuleMap(LAZY_MODULE_MAP)
+  ]
+});
+server.engine('html', configuredNgExpressEngine)
+server.set('view engine', 'html');
+server.set('views', 'dist');
+
+const options:cors.CorsOptions = {
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", "Authorization", "x-xsrf-token"],
+  credentials: true,
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  origin: ['http://localhost:4200', 'http://localhost:8000'],
+  preflightContinue: false,
+  optionsSuccessStatus: 200
+};
+
+server.use(compression());
+server.use('/', express.static('dist', { index: false }));
+server.use('/assets', express.static(path.join(__dirname, 'assets'), { maxAge: '1y' }));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(cookieParser());
+server.use(cors(options));
+server.options("*", cors(options));
+
 async function bootstrap() {
-  const app = await NestFactory.create(ApplicationModule);
-  app.use(bodyParser.json());
+  const app = await NestFactory.create(ApplicationModule, server);
   await app.listen(8000);
 }
 
