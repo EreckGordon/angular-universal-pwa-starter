c910eb2311b1b1025674a22b7df2dbde0660ca8a
EreckGordon <ereckgordon@gmail.com>
2017-11-28 20:43:30 -0700

    csrf middleware check

---
+++ server/modules/common/middlewares/check-csrf-token.middleware.ts
@@ -0,0 +1,10 @@
+export const checkCSRFTokenMiddleware = (req, res, next) => {
+    const csrfCookie = req.cookies["XSRF-TOKEN"];
+    const csrfHeader = req.headers['x-xsrf-token'];
+    if (csrfCookie && csrfHeader && csrfCookie === csrfHeader) {
+        next();
+    }
+    else {
+        res.sendStatus(403);
+    }	
+};
