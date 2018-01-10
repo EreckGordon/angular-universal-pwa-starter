57d42d465f2dbb6d7b5d1aa78a67265869aac503
EreckGordon <ereckgordon@gmail.com>
2018-01-10 09:45:43 -0700

    simple caching for server side rendered routes

--- server/modules/app.controller.ts
+++ server/modules/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get, Request, Response } from '@nestjs/common';
-
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('*')
 export class AppController {
+    renderCache: {} = {};
 
     @Get()
-    public async routesRender( @Request() req: any, @Response() res: any) {
-        res.render('index', { req })
+    public routesRender( @Req() req: Request, @Res() res: Response) {
+
+        /*
+        * add this if you want to blacklist certain routes from being cached
+        if (req.originalUrl.startsWith('/admin')) {
+            return res.render('index', { req })
+        }
+        */
+
+        if (this.renderCache[req.originalUrl]) {
+            return res.send(this.renderCache[req.originalUrl])
+        }
+
+        res.render('index', { req }, (err, html) => {
+            this.renderCache[req.originalUrl] = html;
+            return res.send(html);
+        })
     }
 
 }
