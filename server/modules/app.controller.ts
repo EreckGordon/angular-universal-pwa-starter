a97620bcb7534ae020f342ded0580f6a9e29ef9c
EreckGordon <ereckgordon@gmail.com>
2018-01-10 10:18:59 -0700

    move cache blacklist logic

--- server/modules/app.controller.ts
+++ server/modules/app.controller.ts
@@ -8,21 +8,23 @@ export class AppController {
     @Get()
     public routesRender( @Req() req: Request, @Res() res: Response) {
 
-        /*
-        * add this if you want to blacklist certain routes from being cached
-        if (req.originalUrl.startsWith('/admin')) {
-            return res.render('index', { req })
-        }
-        */
-
         if (this.renderCache[req.originalUrl]) {
             return res.send(this.renderCache[req.originalUrl])
         }
 
         res.render('index', { req }, (err, html) => {
-            this.renderCache[req.originalUrl] = html;
-            return res.send(html);
-        })
+
+        	// prevent caching these routes
+        	if (req.originalUrl.startsWith('/admin')) {
+        		return res.send(html);
+        	}
+
+        	else {
+	            this.renderCache[req.originalUrl] = html;
+	            return res.send(html);
+        	}
+
+        });
     }
 
 }
