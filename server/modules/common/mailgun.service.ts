faf95b730b2a07a9f2e1a7ef51d07228a3c03634
EreckGordon <ereckgordon@gmail.com>
2018-01-27 08:49:07 -0700

    switch mailgun implementation
    switch from nodemailer + nodemailer-mailgun-transport due to broken
    typings. Would not compile, so must switch.

--- server/modules/common/mailgun.service.ts
+++ server/modules/common/mailgun.service.ts
@@ -1,37 +1,30 @@
 import { Component } from '@nestjs/common';
-import * as nodemailer from 'nodemailer';
-import * as mg from 'nodemailer-mailgun-transport';
+
+const mailgun = require('mailgun-js')({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_EMAIL_DOMAIN});
 
 
 @Component()
 export class MailgunService {
 
-    private nodemailerMailgun = nodemailer.createTransport(
-        mg({
-            auth: {
-                api_key: process.env.MAILGUN_API_KEY,
-                domain: process.env.MAILGUN_EMAIL_DOMAIN
-            }
-        })
-    );
-
     async sendPasswordResetEmail({ email, token }: { email: string; token: string; }): Promise<any> {
+        
         const html = `
-    		<div>To reset your password for ${process.env.SITENAME_BASE}, please follow this link:
-    		<a href="${process.env.SITE_URL}/reset-password/?token=${token}&email=${email}">
-    			${process.env.SITE_URL}/reset-password/?email=${email}&token=${token}
-    		</a></div><br>
+            <div>To reset your password for ${process.env.SITENAME_BASE}, please follow this link:
+            <a href="${process.env.SITE_URL}/reset-password/?token=${token}&email=${email}">
+                ${process.env.SITE_URL}/reset-password/?email=${email}&token=${token}
+            </a></div><br>
 
-    		<div>
-    		The link will expire in 10 minutes.
-    		</div>
-    	`
-        return await this.nodemailerMailgun.sendMail({
+            <div>
+            The link will expire in 10 minutes.
+            </div>
+        `
+        return await mailgun.messages().send({
             to: email,
             from: `noreply@${process.env.MAILGUN_EMAIL_DOMAIN}`,
             subject: `Password Reset Request for ${process.env.SITENAME_BASE}`,
             html
-        })
+        })        
     }
+
 
 }
