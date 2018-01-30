b49b7b5589f48ad57053cbb7de44514dfcdcfa4a
EreckGordon <ereckgordon@gmail.com>
2018-01-29 23:12:25 -0700

    account management component

--- src/app/shared/auth/components/account-management/account-management.component.ts
+++ src/app/shared/auth/components/account-management/account-management.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../auth.service';
+import { AuthService, AuthenticatedUser } from '../../auth.service';
 
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/takeUntil';
@@ -13,13 +13,21 @@ import 'rxjs/add/operator/takeUntil';
 export class AccountManagementComponent implements OnInit, OnDestroy {
 
     destroy: Subject<any> = new Subject();
+    user: AuthenticatedUser;
 
     constructor (public auth: AuthService, private router: Router) { }
 
     ngOnInit() {
         this.auth.user$.takeUntil(this.destroy).subscribe(user => {
-            console.log(user)
+            if ((user === null) || (this.auth.isAuthenticatedUser(user) && !user.email)) {
+                return this.router.navigate(['/']);
+            }
+            if (this.auth.isAuthenticatedUser(user)) this.user = user;
         })
+    }
+
+    logout() {
+        this.auth.logout()
     }
 
     ngOnDestroy() {
