d7f050ceb999c4e931294a123ce26fa1a620519f
EreckGordon <ereckgordon@gmail.com>
2018-03-18 11:22:43 -0700

    remove redundant data

--- src/client/app/features/chat/chat.service.ts
+++ src/client/app/features/chat/chat.service.ts
@@ -5,24 +5,19 @@ import { Observable } from 'rxjs/Observable';
 import { fromEvent } from 'rxjs/observable/fromEvent';
 import { Subject } from 'rxjs/Subject';
 
-import { AuthenticatedUser } from '@interfaces/authenticated-user.interface';
-
 import { AuthService } from '../../shared/auth/auth.service';
 
 @Injectable()
 export class ChatService {
-    private userState: AuthenticatedUser = { email: '', id: '', roles: [''], authProviders: [''], isAnonymous: true };
     socket = io('http://localhost:8001/api/auth/gateway');
     recentlyCreatedAnon$;
     destroy = new Subject();
 
     constructor(private auth: AuthService) {
-        console.log('hi');
         this.auth.user$.subscribe(currentUser => {
             this.resetConnection();
             if (currentUser === null) {
             } else if (this.auth.isAuthenticatedUser(currentUser)) {
-                this.userState = currentUser;
                 this.recentlyCreatedAnon$ = fromEvent(this.socket, 'recently-created-anon')
                     .takeUntil(this.destroy)
                     .subscribe(console.log);
@@ -36,11 +31,7 @@ export class ChatService {
         this.socket.connect();
     }
 
-    get uuid() {
-        return this.userState.id;
-    }
-
     emit() {
-        this.socket.emit('recently-created-anon', { uuid: this.uuid });
+        this.socket.emit('recently-created-anon');
     }
 }
