aac789f5ce4d9f9f6695f45454bbdb096c0fbfff
EreckGordon <ereckgordon@gmail.com>
2018-03-18 15:35:21 -0700

    move role guard to gateway namespace
    with exma

--- src/server/modules/auth/auth.gateway.ts
+++ src/server/modules/auth/auth.gateway.ts
@@ -11,8 +11,9 @@ import { AuthCache } from './auth.cache';
 
 @WebSocketGateway({ namespace: 'api/auth/gateway', port: 8001 })
 export class AuthGateway implements NestGateway {
+    requiredRolesForGateway = ['anon', 'user']; // match any role and you have access
     recentlyCreatedAnonEvent = 'recently-created-anon';
-    requiredRolesForRecentlyCreatedAnonEvent = ['anon', 'user']; // if the user has any of these roles then they are authorized to view the stream
+    // requiredRolesForRecentlyCreatedAnonEvent = ['user']; // add this for granular role check
 
     constructor(private readonly authCache: AuthCache, private readonly securityService: SecurityService) {}
 
@@ -22,18 +23,29 @@ export class AuthGateway implements NestGateway {
         const parsedCookies = cookie.parse(client.request.headers.cookie);
         const userCookie = parsedCookies['SESSIONID'];
         if (!userCookie) {
-            return (client.user = { roles: [''] });
+            return client.disconnect();
         }
         const user = await this.securityService.decodeJwt(userCookie);
+        const canAccess = this.roleGuard(user.roles, this.requiredRolesForGateway);
+        if (!canAccess) {
+            return client.disconnect();
+        }
         client.user = user;
     }
 
     @SubscribeMessage('recently-created-anon')
     onRecentlyCreatedAnon(client, data): Observable<WsResponse<any>> {
+        /*
+        * we can also do granular role guarding
+        *
+        * depending on what kind of action this socket is, we may want to check that the user exists in db before performing any action.
+        * however, I have chosen to not check the db for normal type of interactions because the guard does not come without a cost.
+        *
         const hasRole = this.roleGuard(client.user.roles, this.requiredRolesForRecentlyCreatedAnonEvent);
         if (!hasRole) {
             return of({ event: this.recentlyCreatedAnonEvent, data: [] });
         }
+        */
         return this.authCache.wsObservable.pipe(map(res => ({ event: this.recentlyCreatedAnonEvent, data: res })));
     }
 
