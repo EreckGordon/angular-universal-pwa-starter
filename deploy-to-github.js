a28fdbd198ccdcea58bd7b00f608cd0ea7a23512
EreckGordon <ereckgordon@gmail.com>
2018-07-14 14:29:06 -0700

    path.sep more elegant

--- deploy-to-github.js
+++ deploy-to-github.js
@@ -1,18 +1,8 @@
 const path = require('path');
-const os = require('os');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-let pathFolder;
-
-if (os.platform() == 'win32') {
-    // Windows
-    pathFolder = __dirname.split('\\');
-} else {
-    // Linux / MacOS
-    pathFolder = __dirname.split('/');
-}
-
+let pathFolder __dirname.split(path.sep);
 let folderLength = pathFolder.length;
 let currentFolder = pathFolder[folderLength - parseInt(1)];
 
